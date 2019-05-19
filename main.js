0886ee93eddf0b7f86fc9a432b3f7e7e7d6809fa
Nicholas Regoli <nicholasregoli@gmail.com>
2019-05-18 17:26:17 -0700

    displaying 1st page names

---
+++ main.js
@@ -0,0 +1,25 @@
+const namesList = document.getElementById('namesList');
+
+const getSWData = async () => {
+  const response = await fetch("https://swapi.co/api/people/?format=json");
+  const swData = await response.json();
+
+  const arrSW = swData.results.map((x) => {
+    return x;
+  });
+
+  console.log(arrSW);
+
+  const renderNames = () => {
+    return `
+      <ul>
+        ${ arrSW.map(obj =>
+          `<li> ${ obj.name } </li>`
+        ).join('') }
+      </ul>
+    `
+  }
+  namesList.innerHTML = renderNames();
+}
+
+getSWData();
