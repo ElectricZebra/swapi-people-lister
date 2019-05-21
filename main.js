20c932d02fbe5c140fd22345d3ca7f8663e7a526
Nicholas Regoli <nicholasregoli@gmail.com>
2019-05-21 13:40:42 -0700

    click ev starting to work

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const namesList = document.getElementById('namesList');
+const ul = document.getElementById('namesList');
+const details = document.getElementById('details');
 
 const getSWData = async () => {
   const response = await fetch("https://swapi.co/api/people/?format=json");
@@ -7,17 +9,34 @@ const getSWData = async () => {
   const arrSW = swData.results.map((x) => {
     return x;
   });
+  console.log(arrSW);
+
 
   const renderNames = () => {
     return `
-      <ul>
+      <ul id='namesList'>
         ${ arrSW.map(obj =>
-          `<li> ${ obj.name } </li>`
-        ).join('') }
+      `<li> ${obj.name} </li>`
+    ).join('')}
       </ul>
     `
   }
   namesList.innerHTML = renderNames();
+
+
+
+  namesList.addEventListener('click', (ev) => {
+    // console.dir(ev.target)
+    // const curObj = ;
+    const curObj = arrSW.map(obj => {
+      if (obj.name === ev.target.innerText) {
+        console.log(obj)
+        details.innerHTML = `${obj.mass} ${obj.hair_color}`
+      }
+    })
+    // details.innerHTML = curObj
+    // console.dir(curObj)
+  })
 }
 
 getSWData();
